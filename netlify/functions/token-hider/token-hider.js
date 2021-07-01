03b94fc7294cb7ff182fd9d09c938522ec20edfa
JG <68207889+industrialmass@users.noreply.github.com>
2021-07-01 01:56:30 -0500

    Test of changes

--- netlify/functions/token-hider/token-hider.js
+++ netlify/functions/token-hider/token-hider.js
@@ -2,47 +2,48 @@ const process = require("process");
 const fetch = require("node-fetch");
 
 const handler = async function (event) {
-  console.log(fetch);
-
-  /*  // get query parameters
-  const { q } = event.queryStringParameters;
+  // get query parameters
+  const q = event.queryStringParameters.q || "Lubbock,TX";
 
   console.log(`Query parameters: ${q}`);
 
   // Get env var values defined in our Netlify site UI
   // this is secret, our frontend won't see this
-  const { API_URL, API_TOKEN } = process.env;
+  const { API_TOKEN } = process.env;
 
   // First we make a call to match city name to latitude and longitude
-  const coordsURL = `${API_URL}/weather?q=${q}&appid=${API_TOKEN}`;
+  const coordsURL = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${API_TOKEN}`;
 
   const coordsRequest = await fetch(coordsURL, {
     method: "GET",
     mode: "cors",
-  }).catch((error) => console.log(error));
+  }).catch((error) => {
+    console.log(error);
+  });
 
-  const { coords } = await coordsRequest
-    .json()
-    .catch((error) => console.log(error));
+  const newData = await coordsRequest.json().catch((error) => {
+    console.log(error);
+  });
 
   console.log(`coords:`);
-  console.log(coords);
+  console.log(newData);
 
   // Now we can make our real call to the API
-  const URL = `${API_URL}/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${API_TOKEN}`;
+  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${newData.coord.lat}&lon=${newData.coord.lon}&appid=${API_TOKEN}`;
 
   const request = await fetch(URL, {
     method: "GET",
     mode: "cors",
-  }).catch((error) => console.log(error));
+  }).catch((error) => {
+    console.log(error);
+  });
 
   const data = await request.json().catch((error) => console.log(error));
 
   console.log("Data:");
   console.log(data);
 
-  return data;*/
-  return 1;
+  return data;
 };
 
 module.exports = { handler };
