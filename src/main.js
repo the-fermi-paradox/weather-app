3eb8852a3ad5cafd76e7e2c25c56020b491d5ca1
JG <68207889+industrialmass@users.noreply.github.com>
2021-06-30 22:19:19 -0500

    Add webpack, build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { model } from "./model/model";
 
-const log = async(() => {
+const log = async () => {
   const output = await model.get().catch((error) => console.log(error));
   console.log(output);
-});
+};
+
+log();
