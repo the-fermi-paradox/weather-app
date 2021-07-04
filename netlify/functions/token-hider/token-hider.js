fc0e5e250248d644d04927ff8c2b5750e1daa900
JG <joshgundy55@gmail.com>
2021-07-03 21:00:37 -0500

    Refactor token-hider with proper error handling

--- netlify/functions/token-hider/token-hider.js
+++ netlify/functions/token-hider/token-hider.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 const handler = async function (event) {
   // get query parameters
-  const q = event.queryStringParameters.q || "Lubbock,TX";
+  const q = event.queryStringParameters.q || "Lubbock";
 
   console.log(`Query parameters: ${q}`);
 
@@ -10,53 +10,63 @@ const handler = async function (event) {
   // this is secret, our frontend won't see this
   const { API_TOKEN } = process.env;
 
-  // First we make a call to match city name to latitude and longitude
-  const coordsURL = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${API_TOKEN}`;
+  // First we make a call with the user's city name input
+  const coords = await (async () => {  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${API_TOKEN}`;
+    const response = await fetch(url).catch((error) => { throw error });
 
-  const coordsRequest = await fetch(coordsURL, {
-    method: "GET",
-    mode: "cors",
-  }).catch((error) => {
-    throw error;
-  });
+    if (!response.ok) {
+      return {
+          statusCode: response.cod,
+          body: response.message,
+      }
+    }
 
-  if (coordsRequest.cod === "404") {
-    throw coordsRequest.message;
-  } else if (coordsRequest.cod === "401") {
-    throw coordsRequest.message;
+    const data = response.json().catch((error) => { throw error; }) ;
+
+    if (data) {
+      return {
+        statusCode: 200,
+        body: data
+      };
+    } else {
+      return {
+        statusCode: 204,
+        body: "Error parsing response into JSON"
+      }
+    }
+  })().catch((error) => { throw error; });
+
+  if (coords.statusCode !== 200) {
+    throw new Error(`ERROR ${coords.statusCode}: Data failed to be retrieved
+    Message was: ${coords.body}`);
   }
-
-  console.log(coordsRequest);
-
-  const newData = await coordsRequest.json().catch((error) => {
-    console.log(error);
-  });
-
-  if (newData.cod === "404") {
-    throw newData.message;
-  } else if (newData.cod === "401") {
-    throw newData.message;
-  }
-
-  console.log(`coords:`);
-  console.log(newData);
 
   // Now we can make our real call to the API
-  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${newData.coord.lat}&lon=${newData.coord.lon}&appid=${API_TOKEN}`;
+  // using the coordinates from the last call
+  // to get the One Call data
+  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.coord.lat}&lon=${coords.coord.lon}&appid=${API_TOKEN}`;
+  const response = await fetch(URL).catch((error) => { throw error });
+  if (!response.ok) {
+    return {
+      statusCode: response.cod,
+      body: response.message, 
+    }
+  }
 
-  const request = await fetch(URL, {
-    method: "GET",
-    mode: "cors",
-  }).catch((error) => {
-    console.log(error);
-  });
+  const data = await request.json().catch((error) => { throw error });
 
-  const data = await request.json().catch((error) => console.log(error));
-
-  console.log("Data:");
-  console.log(data);
-
-  return data;
+  if (data) {
+    return {
+      statusCode: 200,
+      body: data
+    };
+  } else {
+    return {
+      statusCode: 204,
+      body: "Error parsing response into JSON"
+    }
+  }
 };
 
 module.exports = { handler };
