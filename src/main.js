c1075a6bd2d26407c5ae95f28c03786383c1ae91
JG <joshgundy55@gmail.com>
2021-07-05 22:31:27 -0500

    Add a temp input parameter to the main file for now

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { model } from "./model/model";
 
 const log = async () => {
-  const output = await model.get().catch((error) => console.log(error));
+  const output = await model.get("San Diego").catch((error) => console.log(error));
   console.log(output);
 };
 
