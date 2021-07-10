97f597ab9ea96b92d658d35bcf23cf49854f1c13
JG <joshgundy55@gmail.com>
2021-07-09 19:08:05 -0500

    Deliver a flat object to client

---
+++ src/model/parse-data.js
@@ -0,0 +1,22 @@
+const parseData = (data) => {
+  const flatObject = {};
+  // Our primary purpose here is to flatten
+  // the object as much as we can, so that
+  // our future data fetching isn't burdensome
+  flatObject.temp = data.current.temp;
+  flatObject.high = data.daily[0].temp.max;
+  flatObject.low = data.daily[0].temp.min;
+  flatObject.pop = data.daily[0].pop;
+  flatObject.sunrise = data.current.sunrise;
+  flatObject.sunset = data.current.sunset;
+  flatObject.weather = data.current.weather[0].main;
+  flatObject.windSpeed = data.current.wind_speed;
+  flatObject.iconId = data.current.weather[0].icon;
+  // These arrays will need to be flattened
+  // as they're being used ...
+  flatObject.hourly = data.hourly;
+  flatObject.daily = data.daily;
+  return flatObject;
+};
+
+export default parseData;
